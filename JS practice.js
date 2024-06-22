d38f615a7a471592809455719656a7414278eaa8
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-06-22 14:40:14 +0500

    Create JS practice.js

---
+++ JS practice.js
@@ -0,0 +1,16 @@
+//************************************************VERY SIMPLE CALCULATOR IN JS***************************************************
+let a = 6, b = 5;
+let operation = "+";
+if(operation == "+") {
+    console.log("Addition:",a+b);
+}
+else if(operation == "-") {
+    console.log("Subtraction: ",a-b);
+}
+else if(operation == "*") {
+    console.log("Multiplication:",a*b);
+}
+else if(operation == "/") {
+    console.log("Division:",a/b);
+}
+else console.log("Get out!");
